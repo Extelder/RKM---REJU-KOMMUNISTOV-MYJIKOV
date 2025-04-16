476a5c2ea89d38ca795dc4f60059181ac3be0db9
Extelder <Nikolaoskas15@gmail.com>
2025-04-17 01:03:26 +0700

    Added base hand door open

---
+++ Assets/Scripts/Player/Character/HandDoor/HandDoorOpenChecker.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class HandDoorOpenChecker : MonoBehaviour
+{
+    [SerializeField] private TwoBoneIKConstraint _ik;
+    [SerializeField] private Transform _handTarget;
+    [SerializeField] private float _weightChangeSpeed;
+
+    private float _tagetWeight;
+
+    private void Update()
+    {
+        _ik.weight = Mathf.Lerp(_ik.weight, _tagetWeight, _weightChangeSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            _tagetWeight = 1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            _tagetWeight = 0;
+        }
+    }
+}
