e519f96e1222faa08562f8c163aa7bb969a99622
Extelder <Nikolaoskas15@gmail.com>
2025-04-16 18:00:51 +0700

    fixwatch

--- Assets/Scripts/Player/Character/Watch/CharacterCheckWatch.cs
+++ Assets/Scripts/Player/Character/Watch/CharacterCheckWatch.cs
@@ -7,21 +7,25 @@ using UnityEngine.Animations.Rigging;
 public class CharacterCheckWatch : MonoBehaviour
 {
     [SerializeField] private TwoBoneIKConstraint _ik;
-    [SerializeField] private float _weightChangeSpeed;
+    [SerializeField] private float _upWeightChangeSpeed;
+    [SerializeField] private float _downWeightChangeSpeed;
 
     private float _targetWeight = 0;
+    private float _currentChangeSpeed = 0;
 
     private void Update()
     {
-        _ik.weight = Mathf.Lerp(_ik.weight, _targetWeight, _weightChangeSpeed * Time.deltaTime);
+        _ik.weight = Mathf.Lerp(_ik.weight, _targetWeight, _currentChangeSpeed * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.T))
         {
+            _currentChangeSpeed = _upWeightChangeSpeed;
             _targetWeight = 1;
         }
 
         if (Input.GetKeyUp(KeyCode.T))
         {
+            _currentChangeSpeed = _downWeightChangeSpeed;
             _targetWeight = 0;
         }
     }
