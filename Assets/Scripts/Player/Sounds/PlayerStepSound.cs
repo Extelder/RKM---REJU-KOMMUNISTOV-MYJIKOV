8a0f52af32dabb4af5e55b028aa4d2be81084655
igoryava <126491702+igoryava@users.noreply.github.com>
2025-04-23 14:54:47 +0300

    Added few sounds and fixed step sound

--- Assets/Scripts/Player/Sounds/PlayerStepSound.cs
+++ Assets/Scripts/Player/Sounds/PlayerStepSound.cs
@@ -4,15 +4,27 @@ using System.Collections.Generic;
 using EvolveGames;
 using UniRx;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStepSound : MonoBehaviour
 {
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private AudioSource _stepSound;
+    [SerializeField] private AudioClip _officeStepSound;
+    [SerializeField] private AudioClip _defaultStepSound;
+    [SerializeField] private int _officeSceneIndex;
     private CompositeDisposable _compositeDisposable = new CompositeDisposable();
 
     private void OnEnable()
     {
+        if (SceneManager.GetActiveScene().buildIndex == _officeSceneIndex)
+        {
+            _stepSound.clip = _officeStepSound;
+        }
+        else
+        {
+            _stepSound.clip = _defaultStepSound;
+        }
         _playerController.Moving.Subscribe(_ =>
         {
             if (_)
@@ -29,5 +41,6 @@ public class PlayerStepSound : MonoBehaviour
     private void OnDisable()
     {
         _compositeDisposable.Clear();
+        _stepSound.Stop();
     }
 }
