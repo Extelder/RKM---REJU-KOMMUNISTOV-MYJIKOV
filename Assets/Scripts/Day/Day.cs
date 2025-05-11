8b279013e3e380f4ee2c4d7af0c85c075ef7ff51
Extelder <Nikolaoskas15@gmail.com>
2025-05-11 21:15:32 +0700

    added day

---
+++ Assets/Scripts/Day/Day.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day : MonoBehaviour
+{
+    [SerializeField] private int _number;
+    [SerializeField] private Character[] _characters;
+
+    public void Begin()
+    {
+        
+    }
+
+    public void End()
+    {
+        
+    }
+}
